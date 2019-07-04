8205de46d1124b46fd4d6597cfcc74bd105c9910
user <user@DESKTOP-BP2FCLT>
2019-07-04 15:23:04 +0900

    First

---
+++ Ch04_Start/src/p02_if_switch/IfExample.java
@@ -0,0 +1,11 @@
+package p02_if_switch;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		//Random은 double 타입이다.
+		
+
+	}
+
+}
---
+++ Ch04_Start/src/p03_while_dowhile/ForPrintFrom1to10Example.java
@@ -0,0 +1,10 @@
+package p03_while_dowhile;
+
+public class ForPrintFrom1to10Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
